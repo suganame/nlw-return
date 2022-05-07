9718ca6a2f09608e10f6711b156009ff19946f8a
suganame <gsuganame@gmail.com>
2022-05-07 16:58:31 -0300

    feat: third class

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import { MailAdapter, SendMailData } from "../mail-adapter";
+import nodemailer from 'nodemailer'
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "cc3e1c9866bbfa",
+    pass: "975d439c95524b"
+  }
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: 'Equipe Feedget <oi@feedget.com>',
+      to: 'Guilherme Suganame <gsuganame@gmail.com>',
+      subject,
+      html: body
+    })
+  };
+}
