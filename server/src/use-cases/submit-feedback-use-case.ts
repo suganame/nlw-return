5ccb81288860d92c959903ccd90b5677043c5312
suganame <gsuganame@gmail.com>
2022-05-09 00:35:28 -0300

    Revert "feat: fifth class"
    This reverts commit 45d95e3c4bb1642b37a1c3972c21f6160969bbf2.

--- server/src/use-cases/submit-feedback-use-case.ts
+++ server/src/use-cases/submit-feedback-use-case.ts
@@ -17,18 +17,6 @@ export class SubmitFeedbackUseCase {
   async execute(request: SubmitFeedbackUseCaseRequest) {
     const { type, comment, screenshot } = request
 
-    if (!type) {
-      throw new Error('Type is Required')
-    }
-
-    if (!comment) {
-      throw new Error('Comment is Required')
-    }
-
-    if (screenshot && !screenshot.startsWith('data:image/png;base64')) {
-      throw new Error('Invalid screenshot format.')
-    }
-
     await this.feedbacksRepository.create({
       type,
       comment,
@@ -41,7 +29,6 @@ export class SubmitFeedbackUseCase {
         `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
         `<p>Tipo do feedback: ${type}</p>`,
         `<p>Comentário: ${comment}</p>`,
-        screenshot ? `<img src="${screenshot}" />` : null,
         `</div>`
       ].join('\n'),
     })
