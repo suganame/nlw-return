a81129621e1108bf998085a989983447c13693e1
suganame <gsuganame@gmail.com>
2022-05-09 00:43:15 -0300

    feat: separating projects

--- web/src/components/Loading.tsx
+++
@@ -1,9 +0,0 @@
-import { CircleNotch } from "phosphor-react"
-
-export const Loading = () =>{
-  return (
-    <div className="h-6 w-6 flex items-center justify-center overflow-hidden">
-      <CircleNotch weight="bold" className="w-4 h-4 animate-spin" />
-    </div>
-  )
-}
