cfca08744ea6762d9de7b1da3dff4a30a7e97365
suganame <gsuganame@gmail.com>
2022-05-09 00:56:43 -0300

    feat: changing author on WidgetForm

--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -64,12 +64,12 @@ export function WidgetForm() {
       )}
 
       <footer className="text-xs text-neutral-400">
-        Feito com ♥ pela
+        Feito com ♥ por 
         <a
           className="underline underline-offset-2"
-          href="https://rocketseat.com.br"
+          href="https://github.com/suganame"
         >
-          Rocketseat
+          Guilherme Suganame
         </a>
       </footer>
     </div>
